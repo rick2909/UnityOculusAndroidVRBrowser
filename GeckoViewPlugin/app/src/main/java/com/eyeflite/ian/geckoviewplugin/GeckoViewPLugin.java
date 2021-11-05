13beaba22d3ff38e1202fd62a92fb4596c9bd103
rick2909 <rick29998@gmail.com>
2021-11-05 17:27:06 +0100

    hopfully build app

--- GeckoViewPlugin/app/src/main/java/com/eyeflite/ian/geckoviewplugin/GeckoViewPLugin.java
+++ GeckoViewPlugin/app/src/main/java/com/eyeflite/ian/geckoviewplugin/GeckoViewPLugin.java
@@ -295,14 +295,24 @@ public class GeckoViewPLugin extends Fragment implements GeckoSession.Navigation
 
     }
 
-
-    public void LoadHTML(final String url) {
+    public void LoadPDF(final String pdf) {
         final Activity a = UnityPlayer.currentActivity;
         a.runOnUiThread(new Runnable() {public void run() {
             if (mWebView == null || mWebView.getSession()==null) {
                 return;
             }
-            mWebView.getSession().loadString(url, "text/html");
+            mWebView.getSession().load(new GeckoSession.Loader().data(pdf, "application/pdf"));
+
+        }});
+    }
+
+    public void LoadHTML(final String data) {
+        final Activity a = UnityPlayer.currentActivity;
+        a.runOnUiThread(new Runnable() {public void run() {
+            if (mWebView == null || mWebView.getSession()==null) {
+                return;
+            }
+            mWebView.getSession().load(new GeckoSession.Loader().data(data, "text/html"));
 
         }});
     }
